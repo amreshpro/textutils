2ca02956d9fc878ee192357a071f81fa62cf1500
Amresh Maurya <amresh.terminal@gmail.com>
2024-06-01 09:56:55 +0530

    fix logic issue

--- utils/getNumber.tsx
+++ utils/getNumber.tsx
@@ -2,5 +2,5 @@ export default function getNumber(text:string){
 const num = text.replace(/[^0-9]/g, '')
 console.log(text)
 console.log("num: "+num)
- return text.replace(/[^0-9]/g, '');
+ return text.replace(/[^0-9]/g, '') !='' ? text.replace(/[^0-9]/g, '')  : text
 }
